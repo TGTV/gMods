47448f59eb1cf507614d4e2d12c9328f62f1f3e0
Jonathan <joengel1@live.de>
2016-03-22 16:21:24 +0100

    160322 1620

---
+++ gModsApp/gModsApp/gModsApp/pages/MainPage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace gModsApp
+{
+    class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            Label header = new Label
+            {
+                Text = "Mods",
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            string[] testdaten =
+            {
+                "1",
+                "2",
+                "3"
+            };
+
+            ListView list = new ListView
+            {
+                ItemsSource = testdaten
+            };
+            this.Detail = new NavigationPage();
+            this.Master = new ContentPage
+            {
+                Title = header.Text,
+                Content = new StackLayout
+                {
+                    Spacing = 10,
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        header,
+                        list
+                    }
+                }
+            };
+
+        }
+    }
+}
