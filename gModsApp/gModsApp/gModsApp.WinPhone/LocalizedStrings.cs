55fc636610dc2d8705752c1b680c9a215609fbcd
Jonathan <joengel1@live.de>
2016-03-22 15:09:43 +0100

    1

---
+++ gModsApp/gModsApp/gModsApp.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using gModsApp.WinPhone.Resources;
+
+namespace gModsApp.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
